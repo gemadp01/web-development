423ee4fe16a075a5bf05a42669c234f64f8888e9
Gema Dodi Pranata <gemadp01@gmail.com>
2023-03-27 21:32:24 +0700

    Menambahkan Coder -> style.css, Javascript, To-Do List, jQuery, Bootstrap

---
+++ Fundamental Front-end/Javascript/dom.js
@@ -0,0 +1,50 @@
+// let el = document.getElementById('div-1');
+let el = document.getElementsByTagName('div');
+console.log(el[0]);
+
+// let el = document.getElementsByClassName('div-2');
+// console.log(el[0]);
+
+// let el = document.querySelector('.div-2');
+// console.log(el);
+
+// let el = document.getElementById('div-1');
+// el.innerHTML = 'test';
+// el.innerHTML = '<p>Hello World!</p>';
+
+// const input = document.getElementById('input');
+// input.setAttribute('type', 'checkbox');
+// input.style.borderColor = 'red';
+// input.style.color = 'blue';
+// console.log(input);
+
+// const divEl = document.getElementById('div-1');
+// const pEl = document.createElement('p');
+
+// divEl.appendChild(pEl);
+// pEl.innerHTML = 'Hello this is P';
+// divEl.removeChild(pEl);
+
+// console.log(divEl);
+
+// const hello = document.getElementById('hello');
+
+// hello.addEventListener('mouseenter', function () {
+//   console.log('Mouse enter hello world.txt');
+// });
+
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click', function () {
+//   const boom = document.getElementById('boom');
+//   boom.style.display = 'block';
+// });
+
+// function showBoom() {
+//   const boom = document.getElementById('boom');
+//   boom.style.display = 'block';
+// }
+
+// function addBorder(el) {
+//   console.log(el);
+//   el.style.border = '1px solid red';
+// }
