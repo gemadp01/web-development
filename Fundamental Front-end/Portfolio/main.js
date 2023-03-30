98fc137cf2346185b02553b7020f147be3140081
Gema Dodi Pranata <gemadp01@gmail.com>
2023-03-30 22:15:22 +0700

    add contact

--- Fundamental Front-end/Portfolio/main.js
+++ Fundamental Front-end/Portfolio/main.js
@@ -13,3 +13,23 @@ function toggleDetail(e) {
 
   $(detail).slideToggle();
 }
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = $('#inp_email');
+  const subject = $('#inp_subject');
+  const message = $('#inp_message');
+
+  if (!$(email).val()) {
+    alert('Email is required!');
+  } else if (!$(subject).val()) {
+    alert('Subject is require!');
+  } else if (!$(message).val()) {
+    alert('Message is required!');
+  } else {
+    $(email).val('');
+    $(subject).val('');
+    $(message).val('');
+    alert('Form Submitted');
+  }
+}
